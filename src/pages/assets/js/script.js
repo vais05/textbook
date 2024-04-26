7953540a596d57b7e43777cf8646a27d3ec49fcf
vais05 <123320748+vais05@users.noreply.github.com>
2024-04-27 02:11:25 +0530

    Delete src/pages/assets directory

--- src/pages/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-function confirmSubmission() {
-    // eslint-disable-next-line no-restricted-globals
-    const confirmed = confirm("Are you sure you want to submit?");
-    if (confirmed) {
-        alert("Form submitted.");
-    }
-}
